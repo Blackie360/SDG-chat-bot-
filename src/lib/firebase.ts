831c2022c2dfdf24f86682d4501a93a643c62345
blackie360 <felixkent360@gmail.com>
2024-11-18 23:42:05 +0300

    github auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,13 @@ import {
 import { getDatabase, Database } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3rBYuzeSLOOdRzLOqUjkLwn8nnetggxU",
-  authDomain: "auth-333bc.firebaseapp.com",
-  projectId: "auth-333bc",
-  storageBucket: "auth-333bc.appspot.com",
-  messagingSenderId: "905826499291",
-  appId: "1:905826499291:web:d1fdea4db605b9e9162afe"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
