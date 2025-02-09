f4bdd3ffacfaff2a423b8b303aec026be370266c
blackie360 <felixkent360@gmail.com>
2025-02-09 17:20:16 +0300

    refactor(app): code refactor

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'build', // Custom build output directory
+  /* config options here */
 };
 
 export default nextConfig;
